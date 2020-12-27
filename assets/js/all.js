946bc82b73d97b43ce676db4317c4cb9ac57cbdf
Tim Chou <lolo19920228@gmail.com>
2020-12-28 03:22:14 +0800

    Update 2020-12-27T19:22:09.822Z

---
+++ assets/js/all.js
@@ -0,0 +1,19 @@
+"use strict";
+
+$(document).ready(function () {
+  // console.log('Hello Bootstrap4');
+  $('.moreInfoBtn').click(function () {
+    $('html,body').animate({
+      scrollTop: $('#popularPetSitters').offset().top - 64
+    }, 800);
+  });
+});
+var swiper = new Swiper('.swiper-container', {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  }
+});
+//# sourceMappingURL=all.js.map
